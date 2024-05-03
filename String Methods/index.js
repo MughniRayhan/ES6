59c87aced0d9de64dba98bfe8bc39f5285bc7948
Mughni R. Tisha <126270177+MughniRayhan@users.noreply.github.com>
2024-05-04 00:32:18 +0600

    Add files via upload

---
+++ String Methods/index.js
@@ -0,0 +1,9 @@
+//startWith(searching,position)
+const message = "Today is friday";
+console.log(message.startsWith('Today'))
+
+//endWith
+console.log(message.endsWith('friday'))
+
+//includes
+console.log(message.includes('Friday'))
